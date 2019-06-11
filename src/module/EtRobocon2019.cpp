4cec2a3f7af8034dbc200c231c54d5e45104bf80
harakai <harakai810@yahoo.co.jp>
2019-06-11 11:14:59 +0900

    test-1

--- src/module/EtRobocon2019.cpp
+++ src/module/EtRobocon2019.cpp
@@ -2,10 +2,18 @@
 #include "Controller.h"
 #include "Calibrator.h"
 #include "Display.h"
+#include "Navigator.h"
 
-void EtRobocon2019::start() {
-  Controller controller;
-  Calibrator calibrator(controller);
+void EtRobocon2019::start()
+{
+    Controller controller;
+    // Calibrator calibrator(controller);
 
-  calibrator.calibration();
+    // calibrator.calibration();
+
+    Navigator navigator(controller);
+
+    navigator.move(1000);
+    controller.tslpTsk(1000);
+    navigator.move(-1000);
 }
