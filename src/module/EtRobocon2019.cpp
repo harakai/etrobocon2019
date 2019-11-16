355173eb520a3fac2406537c6b8643be38c8c8ce
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-11-16 18:56:35 +0900

    Fix

--- src/module/EtRobocon2019.cpp
+++ src/module/EtRobocon2019.cpp
@@ -38,7 +38,7 @@ void EtRobocon2019::start()
   //直接ガレージに移動する
   MoveDirectGarage moveDirectGarage(controller, targetBrightness);
   if(isLeftCourse) {
-    //ブロックビンゴを実行する処理を記述
+    moveDirectGarage.moveDirectGarageL();
   } else {
     moveDirectGarage.moveDirectGarageR();  // Rコースの場合はビンゴを行わずにガレージ駐車を行う
   }
